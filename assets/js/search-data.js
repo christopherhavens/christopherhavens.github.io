cd39b0723495de775a74f7b16788b74246aa0b95
christopherhavens <christopher@pmathp.org>
2025-02-01 18:02:53 +0000

    Deploying to gh-pages from @ christopherhavens/christopherhavens.github.io@17231a2caa16ccaf5379d56de03a8477652df579 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,7 +492,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%63%68%72%69%73%74%6F%70%68%65%72@%70%6D%61%74%68%70.%6F%72%67", "_blank");
         },
       },{
         id: 'social-inspire',
